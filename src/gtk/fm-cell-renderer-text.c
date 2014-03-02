b1913084294a27f4600a29b90b61cb0953246d43
Vadim Ushakov <igeekless@gmail.com>
2014-03-02 14:50:33 +0800

    fm-cell-renderer-text.c: implement caching of layout objects to speed up rendering and layouting if text cells

--- src/gtk/fm-cell-renderer-text.c
+++ src/gtk/fm-cell-renderer-text.c
@@ -32,6 +32,94 @@
 
 #include "fm-cell-renderer-text.h"
 #include <libsmfm-core/fm-config.h>
+#include <string.h>
+
+/*****************************************************************************/
+
+typedef struct _LayoutCacheKey
+{
+    guint hash;
+    gchar * text;
+    size_t  text_size;
+    PangoAlignment alignment;
+    PangoWrapMode wrap_mode;
+    gint width;
+    gint height;
+    GtkWidget * widget;
+} LayoutCacheKey;
+
+typedef struct _LayoutCacheValue
+{
+    PangoLayout * layout;
+    PangoFontDescription * font_description;
+    PangoRectangle pixel_extents;
+} LayoutCacheValue;
+
+static gboolean layout_cache_key_equal(LayoutCacheKey * a, LayoutCacheKey * b)
+{
+    return
+    (a->text_size == b->text_size) &&
+    (a->alignment == b->alignment) &&
+    (a->wrap_mode == b->wrap_mode) &&
+    (a->width     == b->width    ) &&
+    (a->height    == b->height   ) &&
+    (a->widget    == b->widget   ) &&
+    (memcmp(a->text, b->text, b->text_size) == 0);
+}
+
+static void layout_cache_key_make_hash(LayoutCacheKey * a)
+{
+    a->hash = g_str_hash(a->text) + a->text_size + a->alignment + a->wrap_mode + a->width + a->height + (guint) a->widget;
+}
+
+static guint layout_cache_key_get_hash(LayoutCacheKey * a)
+{
+    return a->hash;
+}
+
+static void layout_cache_key_free(LayoutCacheKey * a)
+{
+    g_free(a->text);
+    g_free(a);
+}
+
+static void layout_cache_value_free(LayoutCacheValue * a)
+{
+    g_object_unref(a->layout);
+    pango_font_description_free(a->font_description);
+    g_free(a);
+}
+
+static GHashTable * layout_cache;
+
+static void maintain_layout_cache(void)
+{
+    if (0 /* FIXME: should be value from config */)
+    {
+        if (layout_cache)
+        {
+            g_hash_table_unref(layout_cache);
+            layout_cache = NULL;
+        }
+        return;
+    }
+
+    if (!layout_cache)
+    {
+        layout_cache = g_hash_table_new_full(
+            (GHashFunc) layout_cache_key_get_hash,
+            (GEqualFunc) layout_cache_key_equal,
+            (GDestroyNotify) layout_cache_key_free,
+            (GDestroyNotify) layout_cache_value_free);
+    }
+    else
+    {
+        if (g_hash_table_size(layout_cache) > 6000)
+            g_hash_table_remove_all(layout_cache);
+    }
+}
+
+/*****************************************************************************/
 
 static void fm_cell_renderer_text_get_property(GObject *object, guint param_id,
                                                GValue *value, GParamSpec *psec);
@@ -184,45 +272,81 @@ static void add_attr(PangoAttrList * attr_list, PangoAttribute * attr)
   pango_attr_list_insert(attr_list, attr);
 }
 
-static PangoLayout * get_layout(FmCellRendererText * self, GtkWidget * widget, GdkRectangle * available_room)
+static PangoLayout * get_layout(FmCellRendererText * self,
+                                GtkWidget * widget,
+                                GdkRectangle * available_room,
+                                PangoRectangle * pixel_extents)
 {
     gchar * text;
     PangoWrapMode wrap_mode;
     gint wrap_width;
     PangoAlignment alignment;
 
-    g_object_get(G_OBJECT(self),
+    g_object_get((GObject *) self,
                  "wrap-mode" , &wrap_mode,
                  "wrap-width", &wrap_width,
                  "alignment" , &alignment,
                  "text", &text,
                  NULL);
 
-    PangoLayout * layout = gtk_widget_create_pango_layout(widget, text);
-
-    pango_layout_set_alignment(layout, alignment);
-
-    /* Setup the wrapping. */
-    pango_layout_set_wrap(layout, wrap_mode);
     if (wrap_width < 0 && available_room && available_room->width < wrap_width)
         wrap_width = available_room->width;
-    pango_layout_set_width(layout, wrap_width * PANGO_SCALE);
 
+    LayoutCacheValue * cache_value = NULL;
+
+    maintain_layout_cache();
+
+    LayoutCacheKey key;
+    key.text = text;
+    key.alignment = alignment;
+    key.wrap_mode = wrap_mode;
+    key.width = wrap_width;
+    key.height = self->max_height;
+    key.widget = widget;
+
+    if (layout_cache)
+    {
+        key.text_size = strlen(text);
+        layout_cache_key_make_hash(&key);
+        cache_value = (LayoutCacheValue *) g_hash_table_lookup(layout_cache, &key);
+    }
+
+    if (cache_value)
+    {
+        if (pango_font_description_equal(cache_value->font_description, gtk_widget_get_style(widget)->font_desc))
+            goto end;
+    }
+
+    cache_value = g_new0(LayoutCacheValue, 1);
+
+    PangoLayout * layout = cache_value->layout = gtk_widget_create_pango_layout(widget, key.text);
+
+    pango_layout_set_alignment(layout, key.alignment);
+    pango_layout_set_wrap(layout, key.wrap_mode);
+    pango_layout_set_width(layout, key.width * PANGO_SCALE);
     pango_layout_set_ellipsize(layout, PANGO_ELLIPSIZE_END);
-    if (self->max_height > 0)
-    {
-        pango_layout_set_height(layout, self->max_height * PANGO_SCALE);
-    }
-    else if (self->max_height < 0)
-    {
-        pango_layout_set_height(layout, self->max_height);
-    }
-
+    if (key.height > 0)
+        pango_layout_set_height(layout, key.height * PANGO_SCALE);
+    else if (key.height < 0)
+        pango_layout_set_height(layout, key.height);
     pango_layout_set_auto_dir(layout, TRUE);
 
-    g_free(text);
+    pango_layout_get_pixel_extents(cache_value->layout, NULL, &cache_value->pixel_extents);
 
-    return layout;
+    if (layout_cache)
+    {
+        cache_value->font_description = pango_font_description_copy(gtk_widget_get_style(widget)->font_desc);
+        g_hash_table_insert(layout_cache, g_memdup(&key, sizeof(LayoutCacheKey)), cache_value);
+        text = NULL;
+    }
+
+end:
+    g_free(text);
+    g_object_ref(cache_value->layout);
+
+    if (pixel_extents)
+        *pixel_extents = cache_value->pixel_extents;
+    return cache_value->layout;
 }
 
 #if GTK_CHECK_VERSION(3, 0, 0)
@@ -242,7 +366,7 @@ static void fm_cell_renderer_text_render(GtkCellRenderer *cell,
                                          GtkCellRendererState flags)
 #endif
 {
-    FmCellRendererText *self = FM_CELL_RENDERER_TEXT(cell);
+    FmCellRendererText * self = FM_CELL_RENDERER_TEXT(cell);
 
 #if GTK_CHECK_VERSION(3, 0, 0)
     GtkStyleContext* style;
@@ -260,7 +384,7 @@ static void fm_cell_renderer_text_render(GtkCellRenderer *cell,
     GdkRectangle rect;
     gint xpad, ypad;
 
-    g_object_get(G_OBJECT(cell),
+    g_object_get((GObject *) cell,
                  "foreground-set", &foreground_set,
 #if GTK_CHECK_VERSION(3, 0, 0)
                  "foreground-rgba", &foreground_color,
@@ -285,8 +409,8 @@ static void fm_cell_renderer_text_render(GtkCellRenderer *cell,
     xpad = 0;
     ypad = 0;
 
-
-    PangoLayout* layout = get_layout(self, widget, cell_area);
+    PangoRectangle layout_rect;
+    PangoLayout* layout = get_layout(self, widget, cell_area, &layout_rect);
 
     if (foreground_set && foreground_color && (flags & GTK_CELL_RENDERER_SELECTED) == 0)
     {
@@ -306,9 +430,6 @@ static void fm_cell_renderer_text_render(GtkCellRenderer *cell,
         pango_layout_set_attributes(layout, attr_list);
         pango_attr_list_unref(attr_list);
     }
-
-    PangoRectangle layout_rect;
-    pango_layout_get_pixel_extents(layout, NULL, &layout_rect);
 
     {
         rect.x = cell_area->x + x_offset;
@@ -398,8 +519,8 @@ static void fm_cell_renderer_text_render(GtkCellRenderer *cell,
 
     gchar * text = NULL;
     if(flags & GTK_CELL_RENDERER_PRELIT) /* hovered */
-        g_object_get(G_OBJECT(cell), "text", &text, NULL);
-    g_object_set(G_OBJECT(widget), "tooltip-text", text, NULL);
+        g_object_get((GObject *) cell, "text", &text, NULL);
+    g_object_set((GObject *) widget, "tooltip-text", text, NULL);
 
     g_free(text);
 }
@@ -416,9 +537,11 @@ static void fm_cell_renderer_text_get_size(GtkCellRenderer            *cell,
                                            gint                       *height)
 {
     FmCellRendererText *self = FM_CELL_RENDERER_TEXT(cell);
-    PangoLayout * layout = NULL;
 
     int xpad = 2, ypad = 2;
+
+    PangoLayout * layout = NULL;
+    PangoRectangle layout_rect;
 
     if (rectangle)
     {
@@ -432,15 +555,12 @@ static void fm_cell_renderer_text_get_size(GtkCellRenderer            *cell,
             available_room.height -= ypad * 2;
         else
             available_room.height = 0;
-        layout = get_layout(self, widget, &available_room);
+        layout = get_layout(self, widget, &available_room, &layout_rect);
     }
     else
     {
-        layout = get_layout(self, widget, NULL);
+        layout = get_layout(self, widget, NULL, &layout_rect);
     }
-
-    PangoRectangle layout_rect;
-    pango_layout_get_pixel_extents(layout, NULL, &layout_rect);
 
     g_object_unref(layout);
 
